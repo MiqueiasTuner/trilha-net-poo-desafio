ceaec4d62a08ec583264fbcd9dfeb639ae2d2546
MiqueiasTuner <134027020+MiqueiasTuner@users.noreply.github.com>
2025-11-03 12:33:13 -0300

    commit smartphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,24 +1,30 @@
+using System;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine("📞 Ligando...");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine("📲 Recebendo ligação...");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
