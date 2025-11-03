e4697f72f4a19d37ce5ee05d53b9772e14b2d835
MiqueiasTuner <134027020+MiqueiasTuner@users.noreply.github.com>
2025-11-03 12:34:12 -0300

    Implement main method for smartphone testingcommit program com os testes
    Added main method to test Nokia and iPhone classes.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia("99999-1111", "Nokia 3310", "123456789", 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("--------------------------------");
+
+        Smartphone iphone = new Iphone("99999-2222", "iPhone 15", "987654321", 256);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
